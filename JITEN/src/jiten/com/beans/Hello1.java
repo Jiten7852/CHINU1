7ea2a443e9828685be78eee2726dc14df62a6acf
TL <lenovo@LAPTOP-ER3JLGA9>
2021-08-01 16:59:34 +0530

    H

---
+++ JITEN/src/jiten/com/beans/Hello1.java
@@ -0,0 +1,11 @@
+package jiten.com.beans;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I love my country");
+
+	}
+
+}
